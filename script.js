4da972634124423012d44f07f51ce9bae12ad3ca
sanz <sanzgbmi@gmail.com>
2026-01-16 18:34:18 +0800

    Fix duplicate API_KEY declaration breaking site

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const API_KEY = "5ebfe36c5bbfbd1bdb4f1a9080287aca";
-const BASE_URL = "https://api.themoviedb.org/3";
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 /* ================= HERO ================= */
 const HERO_ENDPOINT = "/trending/movie/week";
