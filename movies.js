6c0f7dad5d750d5f5fd4061e908bce41f260cd69
sanz <sanzgbmi@gmail.com>
2026-01-16 18:31:15 +0800

    Fix TV & Movies pages by centralizing globals

--- movies.js
+++ movies.js
@@ -1,7 +1,3 @@
-const API_KEY = "5ebfe36c5bbfbd1bdb4f1a9080287aca";
-const BASE_URL = "https://api.themoviedb.org/3";
-const IMG_URL = "https://image.tmdb.org/t/p/w342";
-
 document.addEventListener("DOMContentLoaded", () => {
     fetchMovies("/movie/popular", "popular");
     fetchMovies("/movie/top_rated", "topRated");
--- tv.js
+++ tv.js
@@ -1,9 +1,4 @@
-const API_KEY = "5ebfe36c5bbfbd1bdb4f1a9080287aca";
-const BASE_URL = "https://api.themoviedb.org/3";
-const IMG_URL = "https://image.tmdb.org/t/p/w342";
 
-const HERO_IMG_DESKTOP = "https://image.tmdb.org/t/p/w1280";
-const HERO_IMG_MOBILE = "https://image.tmdb.org/t/p/w780";
 
 /* ================= FEATURED TV HERO ================= */
 
